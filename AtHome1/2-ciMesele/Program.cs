afbdff9f7dd98fce1d4ce90312d2b93009940044
Zakir Rahimli <60932492+RahimliZakir@users.noreply.github.com>
2020-07-18 06:53:47 +0400

    Add files via upload

---
+++ AtHome1/2-ciMesele/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_ciMesele
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ededi daxil edin: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Cavabiniz: {A}{A}");
+            Console.ReadKey();
+        }
+    }
+}
