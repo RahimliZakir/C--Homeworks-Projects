800f404da98f0c953db5f182223a13a01aa18ef6
Zakir Rahimli <60932492+RahimliZakir@users.noreply.github.com>
2020-07-18 06:44:05 +0400

    Add files via upload

---
+++ C# - Objects (Data Structures)/Object'sSolution/Object'sConsoleApp/MainClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_sConsoleApp
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {   List<OtherHiredOne> bilgiler = new List<OtherHiredOne>();
+
+            var ilkShexsiyyet = new OtherHiredOne("Ilkin", "Agazade", "Evli");
+            bilgiler.Add(ilkShexsiyyet);
+
+            var ikinciShexsiyyet = new OtherHiredOne {
+            ad = "Dadash",
+                soyad = "Nebizade",
+                AileVeziyyeti = "Evli"
+            };
+            bilgiler.Add(ikinciShexsiyyet);
+
+            OtherHiredOne uchuncuShexsiyyet = new OtherHiredOne();
+            uchuncuShexsiyyet.ad = "Zakir";
+            uchuncuShexsiyyet.soyad = "Rahimli";
+            uchuncuShexsiyyet.AileVeziyyeti = "Ayriliq";
+            bilgiler.Add(uchuncuShexsiyyet);
+
+            foreach (var bilgi in bilgiler)
+            {
+                Console.WriteLine($"{bilgi.ad}, {bilgi.soyad}, {bilgi.AileVeziyyeti}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C# - Objects (Data Structures)/Object'sSolution/Object'sConsoleApp/OtherHiredOne.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_sConsoleApp
+{
+    class OtherHiredOne
+    {
+        public OtherHiredOne()
+        {
+
+        }
+
+        public OtherHiredOne(string aad,string asoyad,string aaileveziyyeti)
+        {
+            this.ad = aad;
+            this.soyad = asoyad;
+            this.AileVeziyyeti = aaileveziyyeti;
+        }
+
+        public string ad { get; set; }
+        public string soyad { get; set; }
+
+        public string AileVeziyyeti { get; set; }
+    }
+}
