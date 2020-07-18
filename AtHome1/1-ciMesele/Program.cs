e68137edc2c93c424d3a9c850194bf97e6dbe772
Zakir Rahimli <60932492+RahimliZakir@users.noreply.github.com>
2020-07-18 06:52:21 +0400

    Add files via upload

---
+++ AtHome1/1-ciMesele/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1_ciMesele
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Eded daxil edin: ");
+            string strA = Console.ReadLine();
+            int intA = Convert.ToInt32(strA);
+
+
+            Console.WriteLine($"7{intA}8");
+            Console.ReadKey();
+        }
+    }
+}
